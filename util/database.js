60799fb416f6fbb25cfd1305a8dade0e9c96bd78
Ahmed Aladdin <ahmed3laa3132@gmail.com>
2025-12-30 21:58:28 +0200

    Install mongoDB and connect

--- util/database.js
+++ util/database.js
@@ -1,8 +1,12 @@
-const Sequelize = require("sequelize");
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.g7wrqc5.mongodb.net/?appName=Cluster0`;
 
-const sequelize = new Sequelize("node_shop_app", "root", "root", {
-  dialect: "mysql",
-  host: "localhost",
-});
-
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect(uri)
+    .then((client) => {
+      console.log('MongoDB is connected!');
+      callback(client);
+    })
+    .catch((e) => console.log(e));
+};
+module.exports = mongoConnect;
