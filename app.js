67a8f69a116c692a7d1a6adfd43b8db8536209a4
Ahmed Aladdin <ahmed3laa3132@gmail.com>
2026-01-06 21:10:06 +0200

    Adding and using User model in mongoose

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-// const User = require("./models/user");
+const User = require("./models/user");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -16,14 +16,14 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("695848c8376388bcea4782fe")
-//     .then((user) => {
-//       req.user = new User(user.userName, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("695d5d59b09c32a3886f6e5b")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -34,6 +34,12 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.g7wrqc5.mongodb.net/shop?retryWrites=true&appName=Cluster0`
   )
   .then((r) => {
+    // const user = new User({
+    //   name: "Ahmed Aladdin",
+    //   email: "ahmed3laa3132@gmail.com",
+    //   cart: { items: [] },
+    // });
+    // user.save();
     app.listen(3000, "localhost");
   })
   .catch((e) => console.log(e));
