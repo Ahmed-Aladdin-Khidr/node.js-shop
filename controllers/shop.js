7a79e33a72003bc88df024e077e28672322cc1c8
Ahmed Aladdin <ahmed3laa3132@gmail.com>
2026-01-06 21:35:12 +0200

    Loading the Cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -38,18 +38,19 @@ exports.getIndex = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-// exports.getCart = (req, res, next) => {
-//   req.user
-//     .getCart()
-//     .then((products) => {
-//       res.render("shop/cart", {
-//         path: "/cart",
-//         pageTitle: "Your Cart",
-//         products: products,
-//       });
-//     })
-//     .catch((e) => console.log(e));
-// };
+exports.getCart = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .then((user) => {
+      const products = user.cart.items;
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: products,
+      });
+    })
+    .catch((e) => console.log(e));
+};
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
