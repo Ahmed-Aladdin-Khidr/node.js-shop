687aa757de60269d391ea789a83132be9f9cf847
Ahmed Aladdin <ahmed3laa3132@gmail.com>
2026-01-07 19:19:42 +0200

    Using the Session Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,12 @@
 exports.getLogin = (req, res, next) => {
-    const cookies = req.get("Cookie").split(';').reduce((res, c) => {
-        const [key, val] = c.trim().split('=').map(decodeURIComponent);
-        res[key] = val;
-        return res;
-    }, {});
-  const loginCookie = cookies['loggedIn'] === 'true';
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: loginCookie,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true");
+  req.session.isLoggedIn = true;
   res.redirect("/");
 };
